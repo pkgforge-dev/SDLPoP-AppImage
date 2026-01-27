09a6887f36baaafe649f0b95ecab1d6f0d31bba9
Link4Electronics <link4electronics@gmail.com>
2026-01-27 09:19:15 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,7 +14,6 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/prince
-#mv /opt/sdlpop/* ./AppDir/bin
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
 # Additional changes can be done in between here
