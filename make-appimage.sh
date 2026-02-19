aa730f82fa9ccee8c59135344528fa54944fadf2
Link4Electronics <link4electronics@gmail.com>
2026-02-19 03:15:19 -0300

    add WMCLASS

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/NagyD/SDLPoP/refs/heads/master/data/icon.png
 export DESKTOP=/usr/share/applications/sdlpop.desktop
+export STARTUPWMCLASS=prince
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
