11a2e6c860c04cf2f76ce9329023b8d316e3cf22
Link4Electronics <link4electronics@gmail.com>
2026-04-06 23:12:19 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,7 +9,6 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=https://raw.githubusercontent.com/NagyD/SDLPoP/refs/heads/master/data/icon.png
-export STARTUPWMCLASS=prince
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
