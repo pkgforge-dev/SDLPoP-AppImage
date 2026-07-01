852f7542344a4cb5f6cd2e03f8777840b24301a8
Link4Electronics <link4electronics@gmail.com>
2026-07-01 08:48:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,13 +6,20 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libdecor
+pacman -Syu --noconfirm sdl2_mixer
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
-make-aur-package sdlpop
+echo "Building SDLPoP..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/NagyD/SDLPoP"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./SDLPoP
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
+mkdir -p ./AppDir/bin
+cd ./SDLPoP/src
+make -j$(nproc)
+mv -v ../prince ../data ../SDLPoP.ini ../../AppDir/bin
