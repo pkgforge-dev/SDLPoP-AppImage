121ec1176c4880a12c9a990053fd444a8adcb8f3
Link4Electronics <link4electronics@gmail.com>
2026-08-27 16:32:00 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,10 +12,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano libdecor-mini sdl2_image-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package sdlpop
-
-# If the application needs to be manually built that has to be done down here
 echo "Building SDLPoP..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/NagyD/SDLPoP"
