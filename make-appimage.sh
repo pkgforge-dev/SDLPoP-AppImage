0ade807934182b28e09edfb79f2f26c53bd25c9f
Link4Electronics <link4electronics@gmail.com>
2026-08-27 16:05:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,8 +3,7 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q sdlpop | awk '{print $2; exit}')
-export ARCH VERSION
+export ARCH
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
@@ -12,8 +11,6 @@ export ICON=https://raw.githubusercontent.com/NagyD/SDLPoP/refs/heads/master/dat
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-mkdir -p ./AppDir/bin
-mv /opt/sdlpop/* ./AppDir/bin
 quick-sharun ./AppDir/bin/prince /usr/lib/libSDL2_image-2.0.so.0
 echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
